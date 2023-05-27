908b9e1488e8d6ee9e1047cca556dc047cb166d8
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2023-05-27 00:23:29 +0000

    :package: Updates dependencies to enable mui ToggleButton
    :package: updates React to version 18.

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
@@ -9,4 +9,8 @@ import "../styles/index.css";
 import Layout from "./layout";
 
 //render your react application
-ReactDOM.render(<Layout />, document.querySelector("#app"));
+const root = createRoot(document.querySelector("#app"))
+
+root.render(
+    <Layout />
+);
--- src/front/js/pages/Main.js
+++ src/front/js/pages/Main.js
@@ -1,6 +1,6 @@
 import * as React from "react";
-import ToggleButton from '@mui/core/ToggleButton';
-import ToggleButtonGroup from '@mui/core/ToggleButtonGroup';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export const Main = () => {
     return (
