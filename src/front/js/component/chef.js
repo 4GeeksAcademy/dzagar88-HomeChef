e258d67f31320983c4cea367c83cde685c70cdb7
Dan Zagar <122122116+dzagar88@users.noreply.github.com>
2023-06-21 14:13:16 +0000

    made bg fixed

--- src/front/js/component/chef.js
+++ src/front/js/component/chef.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
 import { MenuItem } from "../component/menuItem.js";
 import { Context } from "../store/appContext.js";
-import homechefBG from "../../img/homechefBG.jpg"
+import homechefBG from "../../img/homechefBG.jpg";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useNavigate } from "react-router-dom";
+
 export const Chef = () => {
     const { store, actions } = useContext(Context);
     const [menuItems, setMenuItems] = useState([]);
     const [menuItemsMap, setMenuItemsMap] = useState({});
     const [showMenuItemForm, setShowMenuItemForm] = useState(false);
+    const [addButtonLabel, setAddButtonLabel] = useState("Add"); // Added state for button label
+
     const addMenuItem = (newMenuItem) => {
         setMenuItems((prevItems) => [...prevItems, newMenuItem]);
         setMenuItemsMap((prevMap) => ({
@@ -18,21 +21,28 @@ export const Chef = () => {
         }));
         setShowMenuItemForm(false);
     };
+
     const toggleMenuItemForm = () => {
         setShowMenuItemForm(!showMenuItemForm);
+        setAddButtonLabel(showMenuItemForm ? "Add" : "Cancel"); // Toggle button label
     };
+
     useEffect(() => {
-        if (!store.token) return
-        actions.getMenuItems()
+        if (!store.token) return;
+        actions.getMenuItems();
     }, [store.token]);
+
     useEffect(() => {
-        actions.getMenuItems()
-    }, [])
+        actions.getMenuItems();
+    }, []);
+
     const [alignment, setAlignment] = React.useState('chef');
     const navigate = useNavigate();
+
     const handleChange = (event, newAlignment) => {
         setAlignment(newAlignment);
     };
+
     const handleClick = (value) => {
         console.log(`Button ${value} clicked!`);
         if (value === "home") {
@@ -45,14 +55,16 @@ export const Chef = () => {
     };
 
     return (
-        <div style={{
-            backgroundImage: `url(${homechefBG})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
-            backgroundAttachment: "fixed"
-            // height: "930px"
-        }}><br />
+        <div
+            style={{
+                backgroundImage: `url(${homechefBG})`,
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundAttachment: "fixed"
+            }}
+        >
+            <br />
 
             <ToggleButtonGroup
                 color="primary"
@@ -62,32 +74,57 @@ export const Chef = () => {
                 aria-label="Platform"
                 className="Toggle d-flex justify-content-center"
             >
-
-                <ToggleButton className="bg-light border" value="diner" onClick={() => handleClick('diner')}>Diner</ToggleButton>
-                <ToggleButton className="bg-light border" value="chef" onClick={() => handleClick('chef')}>Chef</ToggleButton>
+                <ToggleButton
+                    className="bg-light border"
+                    value="diner"
+                    onClick={() => handleClick('diner')}
+                >
+                    Diner
+                </ToggleButton>
+                <ToggleButton
+                    className="bg-light border"
+                    value="chef"
+                    onClick={() => handleClick('chef')}
+                >
+                    Chef
+                </ToggleButton>
             </ToggleButtonGroup>
 
-            <div className="container"><br></br>
+            <div className="container">
+                <br />
                 <h1
                     style={{ width: "27%" }}
-                    className="white-title title-font opacity60 d-flex justify-content-center m-auto shadow1">Chef</h1><br></br>
+                    className="white-title title-font opacity60 d-flex justify-content-center m-auto shadow1"
+                >
+                    Chef
+                </h1>
+                <br />
                 <div
                     style={{ fontSize: "20px", width: "85%" }}
-                    className="white-title title-font opacity60 m-auto p-1 d-flex justify-content-center shadow1">Click "Add" to add a menu item:</div>
+                    className="white-title title-font opacity60 m-auto p-1 d-flex justify-content-center shadow1"
+                >
+                    Click "{addButtonLabel}" to {showMenuItemForm ? "cancel adding" : "add"} a menu item:
+                </div>
                 <div className="d-flex justify-content-center">
                     <div>
                         <button
                             className="container btn white-title oy-button my-2 shadow1 title-font"
-                            onClick={toggleMenuItemForm}>Add</button>
+                            onClick={toggleMenuItemForm}
+                        >
+                            {addButtonLabel}
+                        </button>
                         {showMenuItemForm && <MenuItem addMenuItem={addMenuItem} />}
                     </div>
                 </div>
-
             </div>
+
             {store.menuItems.map(item => (
                 <div key={item.id}>
-                    <div key={item.id} className="container card title-font border border-dark"
-                        style={{ width: "20rem" }}>
+                    <div
+                        key={item.id}
+                        className="container card title-font border border-dark my-3"
+                        style={{ width: "20rem" }}
+                    >
                         <h3>{item.title}</h3>
                         <img src={item.image} alt="Menu Item" />
                         <p><strong><u>Description:</u></strong> {item.description}</p>
@@ -102,8 +139,7 @@ export const Chef = () => {
                         <p><strong><u>State:</u></strong> {item.state}</p>
                     </div><br />
                 </div>
-            ))
-            }
-        </div >
+            ))}
+        </div>
     );
-}
+};
